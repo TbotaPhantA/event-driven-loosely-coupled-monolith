59aae31e6d1d632d2ab05f4c73fba56ed94dde30
TbotaPhantA <botakiller@mail.ru>
2023-11-24 21:56:27 +0000

    class-validator decorators were added to createSalesProduct command.

--- apps/backend/src/sales/domain/salesProduct/commands/createSalesProduct.ts
+++ apps/backend/src/sales/domain/salesProduct/commands/createSalesProduct.ts
@@ -1,7 +1,35 @@
+import {
+  IsNumber,
+  IsString,
+  Length,
+  Max,
+  MaxLength,
+  Min,
+} from 'class-validator';
+import { NoMethods } from '../../../../infrastructure/shared/types/noMethods';
+import {
+  MAX_INT_32,
+  MAX_PRODUCT_DESCRIPTION_LENGTH,
+  MAX_PRODUCT_NAME_LENGTH,
+  MIN_PRODUCT_NAME_LENGTH,
+} from '../../../../infrastructure/shared/constants';
+
 export class CreateSalesProduct {
-  constructor(
-    readonly name: string,
-    readonly description: string,
-    readonly price: number,
-  ) {}
+  @IsString()
+  @Length(MIN_PRODUCT_NAME_LENGTH, MAX_PRODUCT_NAME_LENGTH)
+  public readonly name: string;
+
+  @IsString()
+  @MaxLength(MAX_PRODUCT_DESCRIPTION_LENGTH)
+  public readonly description: string;
+
+  @IsNumber()
+  @Min(0)
+  @Max(MAX_INT_32)
+  public readonly price: number;
+  constructor(raw: NoMethods<CreateSalesProduct>) {
+    this.name = raw.name;
+    this.description = raw.description;
+    this.price = raw.price;
+  }
 }
