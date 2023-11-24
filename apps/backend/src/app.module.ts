821bbd34dd467715e3e746c436bc50eec7e53a52
TbotaPhantA <botakiller@mail.ru>
2023-11-24 23:47:09 +0000

    minor fixes

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SalesModule } from './sales/application/sales.module';
 import { StorageModule } from './storage/application/storage.module';
+import { DatabaseModule } from './infrastructure/db/database.module';
 
 @Module({
-  imports: [SalesModule, StorageModule],
+  imports: [SalesModule, StorageModule, DatabaseModule],
   controllers: [],
   providers: [],
 })
