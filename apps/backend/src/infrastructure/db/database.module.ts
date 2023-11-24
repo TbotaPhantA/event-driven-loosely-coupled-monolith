c7a7e92f366c273dc07595f6d9e9a38c811b4a09
TbotaPhantA <botakiller@mail.ru>
2023-11-24 23:38:13 +0000

    typeorm was initialized

--- apps/backend/src/infrastructure/db/database.module.ts
+++ apps/backend/src/infrastructure/db/database.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from '../config/config';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: config.database.host,
+      port: config.database.port,
+      username: config.database.username,
+      password: config.database.password,
+      database: config.database.database,
+      entities: [],
+      synchronize: config.database.synchronize,
+    }),
+  ],
+})
 export class DatabaseModule {}
