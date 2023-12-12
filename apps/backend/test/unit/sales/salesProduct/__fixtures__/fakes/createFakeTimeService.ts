32cfff594c9f9223666475d61f67ac012265c18f
TbotaPhantA <botakiller@mail.ru>
2023-12-12 20:22:50 +0000

    unit tests were moved to `unit` folder

--- apps/backend/test/unit/sales/salesProduct/__fixtures__/fakes/createFakeTimeService.ts
+++ apps/backend/test/unit/sales/salesProduct/__fixtures__/fakes/createFakeTimeService.ts
@@ -1,4 +1,4 @@
-import { TimeService } from '../../../../../src/infrastructure/time/time.service';
+import { TimeService } from '../../../../../../src/infrastructure/time/time.service';
 
 export const createFakeTimeService = (): Record<keyof TimeService, jest.Mock> => ({
   now: jest.fn().mockReturnValue(new Date(2022, 0, 3)),
