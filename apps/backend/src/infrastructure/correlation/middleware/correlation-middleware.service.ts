70e1e962871bdb5a062abab656f811a003a2e5ba
TbotaPhantA <botakiller@mail.ru>
2023-12-24 00:57:08 +0000

    minor middleware fixes

--- apps/backend/src/infrastructure/correlation/middleware/correlation-middleware.service.ts
+++ apps/backend/src/infrastructure/correlation/middleware/correlation-middleware.service.ts
@@ -1,14 +1,14 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import * as http from 'http';
 import { CorrelationService } from '../correlation.service';
 import { CORRELATION_ID_HEADER } from '../correlationConstants';
+import { Request, Response } from 'express';
 
 @Injectable()
 export class CorrelationMiddleware implements NestMiddleware {
   constructor(private readonly correlationService: CorrelationService) {}
 
-  public use(req: http.IncomingMessage, res: http.ServerResponse, next: () => void): void {
-    const correlationIdHeader = req.headers[CORRELATION_ID_HEADER];
+  public use(req: Request, res: Response, next: () => void): void {
+    const correlationIdHeader = req.get(CORRELATION_ID_HEADER);
 
     req.headers[CORRELATION_ID_HEADER] = this.correlationService.startNewCorrelationId(correlationIdHeader);
 
