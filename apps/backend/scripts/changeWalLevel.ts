7c101945840b020205e4beee1fe854b44b9d01c8
TbotaPhantA <botakiller@mail.ru>
2024-01-09 22:35:18 +0000

    script which changes wal_level was added, because it's required by `debezium`.

---
+++ apps/backend/scripts/changeWalLevel.ts
@@ -0,0 +1,23 @@
+import * as dotenv from 'dotenv';
+import { config } from '../src/infrastructure/config/config';
+import { Client } from 'pg';
+
+dotenv.config({ path: '.env.test.local' });
+
+(async (): Promise<void> => {
+  const { database, username, password, host, port } = config.database;
+  const connectionString = `postgresql://${username}:${password}@${host}:${port}/${database}`;
+
+  const client = new Client({ connectionString });
+  await client.connect();
+
+  try {
+    const query = `ALTER SYSTEM SET wal_level = logical;`;
+    await client.query(query);
+    console.log('wal_level changed to logical');
+  } catch (e) {
+    console.error('Error creating connector:', e);
+  } finally {
+    await client.end();
+  }
+})()
