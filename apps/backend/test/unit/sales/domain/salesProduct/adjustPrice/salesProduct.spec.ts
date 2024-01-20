5fd5189d2ba272e0583b4a2095ab35c0a0e6ddd1
TbotaPhantA <botakiller@mail.ru>
2024-01-20 21:04:29 +0000

    `adjustPrice` unit tests was added for sales product domain object

---
+++ apps/backend/test/unit/sales/domain/salesProduct/adjustPrice/salesProduct.spec.ts
@@ -0,0 +1,38 @@
+import { SalesProductBuilder } from '../../../../../__fixtures__/builders/salesProduct.builder';
+import { TimeService } from '../../../../../../src/infrastructure/time/time.service';
+import { mock } from 'jest-mock-extended';
+import { AdjustPriceBuilder } from '../../../../../__fixtures__/builders/commands/adjustPrice.builder';
+
+describe('SalesProduct', () => {
+  const now = new Date(2022, 0, 4);
+  const mockTimeService = mock<TimeService>();
+
+  beforeEach(() => {
+    mockTimeService.now.mockReturnValue(now);
+  })
+
+  describe('adjustPrice', () => {
+    const testCases = [
+      {
+        toString: (): string => '1',
+        product: SalesProductBuilder.defaultAll.with({
+          price: 500,
+          updatedAt: new Date(2022, 0, 3),
+        }).result,
+        command: AdjustPriceBuilder.defaultAll.with({
+          newPrice: 1000,
+        }).result,
+        expectedProduct: SalesProductBuilder.defaultAll.with({
+          price: 1000,
+          updatedAt: now,
+        }).result
+      },
+    ];
+
+    test.each(testCases)('%s', ({ product, command, expectedProduct }) => {
+      product.adjustPrice(command, { time: mockTimeService });
+
+      expect(product.export()).toStrictEqual(expectedProduct.export());
+    });
+  });
+});
