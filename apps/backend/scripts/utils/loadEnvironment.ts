d0b29f5013d87386b3e12e2ea4fbf8749a5b84d4
TbotaPhantA <botakiller@mail.ru>
2024-03-20 20:10:50 +0000

    playground was added

--- apps/backend/scripts/utils/loadEnvironment.ts
+++ apps/backend/scripts/utils/loadEnvironment.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv';
 
-export function loadEnvironment() {
+export function loadEnvironment(): void {
   if (!!process.argv.find((arg) => arg === "--acceptance-tests")) {
     dotenv.config({ path: '.env.test.local' });
   } else {
