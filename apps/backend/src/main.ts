d8f847f3745ae3e70375e7eb8487ea2a4f595b7e
TbotaPhantA <botakiller@mail.ru>
2024-05-06 07:19:40 +0000

    kafka message consuming was added

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { config } from './infrastructure/config/config';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,24 @@ async function bootstrap(): Promise<void> {
   const document = SwaggerModule.createDocument(app, documentConfig);
   SwaggerModule.setup('api', app, document);
 
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: [
+          `${config.kafka.kafka1Host}:${config.kafka.kafka1ExternalPort}`,
+          `${config.kafka.kafka2Host}:${config.kafka.kafka2ExternalPort}`,
+          `${config.kafka.kafka3Host}:${config.kafka.kafka3ExternalPort}`,
+        ],
+        connectionTimeout: 10000,
+      },
+      consumer: {
+        groupId: config.kafka.consumerGroup,
+      },
+    },
+  }, { inheritAppConfig: true });
+
+  await app.startAllMicroservices()
   await app.listen(config.app.port, () => {
     console.log('Application started on: ', config.app.origin);
   });
