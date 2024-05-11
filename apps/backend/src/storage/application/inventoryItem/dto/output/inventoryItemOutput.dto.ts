0d296795614cf0f4231c48d65a27ff147db5986c
TbotaPhantA <botakiller@mail.ru>
2024-05-11 20:00:14 +0000

    swagger decorators were added

--- apps/backend/src/storage/application/inventoryItem/dto/output/inventoryItemOutput.dto.ts
+++ apps/backend/src/storage/application/inventoryItem/dto/output/inventoryItemOutput.dto.ts
@@ -1,12 +1,26 @@
 import { NoMethods } from '../../../../../infrastructure/shared/types/noMethods';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class InventoryItemOutputDto {
+  @ApiProperty()
   readonly inventoryItemId: string;
+
+  @ApiProperty()
   readonly name: string;
+
+  @ApiProperty()
   readonly description: string;
+
+  @ApiProperty()
   readonly quantity: number;
+
+  @ApiProperty()
   readonly createdAt: Date;
+
+  @ApiProperty()
   readonly updatedAt: Date;
+
+  @ApiProperty({ example: null })
   readonly removedAt: Date | null;
 
   constructor(raw: NoMethods<InventoryItemOutputDto>) {
