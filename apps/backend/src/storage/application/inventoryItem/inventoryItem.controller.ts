61a59c943824a6eff5409339229697b956c8a5f0
TbotaPhantA <botakiller@mail.ru>
2024-05-11 20:02:15 +0000

    GET method for getting all inventory items was added

--- apps/backend/src/storage/application/inventoryItem/inventoryItem.controller.ts
+++ apps/backend/src/storage/application/inventoryItem/inventoryItem.controller.ts
@@ -1,13 +1,27 @@
-import { Controller, UseFilters } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Query, UseFilters } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Validate } from '../../../infrastructure/shared/decorators/validate';
 import { HttpExceptionFilter } from '../../../infrastructure/shared/exceptionFilters/httpException.filter';
 import { storageInventoryItemResource } from '../shared/resources';
+import { GetAllInventoryItemsOutputDto } from './dto/output/getAllInventoryItemsOutput.dto';
+import { GetAllInventoryItemsDto } from './dto/input/getAllInventoryItems.dto';
+import { InventoryItemReadService } from './services/inventoryItemRead.service';
 
 @Controller(storageInventoryItemResource)
 @ApiTags(storageInventoryItemResource)
 @Validate()
 @UseFilters(HttpExceptionFilter)
 export class InventoryItemController {
-  async getAllInventoryItems(): Promise<void> { }
+  constructor(
+    private readonly inventoryItemReadService: InventoryItemReadService,
+  ) {}
+
+  @Get()
+  @ApiOperation({ summary: 'Get all inventory items by filter' })
+  @ApiResponse({ type: GetAllInventoryItemsOutputDto })
+  async getAllInventoryItems(
+    @Query() queryParams: GetAllInventoryItemsDto,
+  ): Promise<GetAllInventoryItemsOutputDto> {
+    return this.inventoryItemReadService.getAllInventoryItems(queryParams);
+  }
 }
