fa11a2dc99acc7873dd4b4d3d616fedb4af2f796
TbotaPhantA <botakiller@mail.ru>
2024-05-11 16:17:28 +0000

    `inventoryItemCreate.service.ts` was initialized and implemented

---
+++ apps/backend/src/storage/application/inventoryItem/services/inventoryItemCreate.service.ts
@@ -0,0 +1,36 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ITransaction } from '../../../../infrastructure/transaction/shared/types/ITransaction';
+import { CreateInventoryItemDto } from '../dto/input/createInventoryItem.dto';
+import { TRANSACTION_SERVICE } from '../../../../infrastructure/transaction/shared/constants';
+import { ITransactionService } from '../../../../infrastructure/transaction/ITransaction.service';
+import { InventoryItem } from '../../../domain/inventoryItem/inventoryItem';
+import { TimeService } from '../../../../infrastructure/time/time.service';
+import { InventoryItemRepository } from '../../../dal/inventoryItem.repository';
+import { CreateInventoryItemOutputDto } from '../dto/output/createInventoryItemOutput.dto';
+import { INVENTORY_ITEM_REPOSITORY } from '../../shared/constants';
+
+@Injectable()
+export class InventoryItemCreateService {
+  constructor(
+    @Inject(TRANSACTION_SERVICE)
+    private readonly transactionService: ITransactionService,
+    @Inject(INVENTORY_ITEM_REPOSITORY)
+    private readonly repo: InventoryItemRepository,
+    private readonly time: TimeService,
+  ) {}
+
+  async runTransaction(dto: CreateInventoryItemDto): Promise<CreateInventoryItemOutputDto> {
+    return this.transactionService.withTransaction('SERIALIZABLE', manager => {
+      return this.create(dto, manager);
+    })
+  }
+
+  async create(
+    dto: CreateInventoryItemDto,
+    transaction: ITransaction,
+  ): Promise<CreateInventoryItemOutputDto> {
+    const inventoryItem = InventoryItem.create(dto, { time: this.time });
+    const insertedItem = await this.repo.insert(inventoryItem, transaction);
+    return CreateInventoryItemOutputDto.from(insertedItem);
+  }
+}
