553ff4364b03c959660b3fa007331ff46335ea41
TbotaPhantA <botakiller@mail.ru>
2024-05-11 16:18:37 +0000

    new items were added to DI container

--- apps/backend/src/storage/application/storage.module.ts
+++ apps/backend/src/storage/application/storage.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { InventoryItemController } from './inventoryItem/inventoryItem.controller';
 import { SalesMessagesController } from '../acl/salesMessages.controller';
+import { InventoryItemCreateService } from './inventoryItem/services/inventoryItemCreate.service';
+import { INVENTORY_ITEM_REPOSITORY } from './shared/constants';
+import { DatabaseInventoryItemRepository } from '../dal/databaseInventoryItem.repository';
+import { TransactionModule } from '../../infrastructure/transaction/transaction.module';
+import { TimeModule } from '../../infrastructure/time/time.module';
+import { SalesACLService } from '../acl/salesACL.service';
 
 @Module({
+  imports: [TransactionModule, TimeModule],
   controllers: [InventoryItemController, SalesMessagesController],
+  providers: [
+    {
+      provide: INVENTORY_ITEM_REPOSITORY,
+      useClass: DatabaseInventoryItemRepository,
+    },
+    InventoryItemCreateService,
+    SalesACLService,
+  ],
 })
 export class StorageModule {}
