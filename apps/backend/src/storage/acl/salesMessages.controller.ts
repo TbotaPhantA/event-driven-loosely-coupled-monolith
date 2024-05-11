c6180cee73cdaa4a3b323584c492dda61b7a3f60
TbotaPhantA <botakiller@mail.ru>
2024-05-11 16:18:26 +0000

    TODO was completed and replaced by the implemntation

--- apps/backend/src/storage/acl/salesMessages.controller.ts
+++ apps/backend/src/storage/acl/salesMessages.controller.ts
@@ -6,20 +6,26 @@ import { MessageNames } from '../application/shared/enums/messageNames.enum';
 import { validatePayload } from '../../infrastructure/shared/utils/validatePayload';
 import {
   CreateSalesProductEventPayload
-} from '../application/inventoryItem/dto/input/createSalesProductEventPayload.dto';
+} from './dto/input/createSalesProductEventPayload.dto';
+import { SalesACLService } from './salesACL.service';
+import { InventoryItemCreateService } from '../application/inventoryItem/services/inventoryItemCreate.service';
 
 @Controller()
 export class SalesMessagesController {
+  constructor(
+    private readonly salesACLService: SalesACLService,
+    private readonly inventoryItemCreateService: InventoryItemCreateService,
+  ) {}
+
   @EventPattern(config.kafka.kafkaProductsTopic)
   async handleSalesProductEvent(@Ctx() context: KafkaContext): Promise<void> {
     const message = getMessageByContext(context);
     const plainPayload = JSON.parse(message.value.payload);
 
     if (message.headers.messageName === MessageNames.ProductCreated) {
-      const payload = await validatePayload(CreateSalesProductEventPayload, plainPayload);
-      // const dto = this.salesACL.
-      // TODO: create inventory item via ACL
-      console.log({ payload });
+      const event = await validatePayload(CreateSalesProductEventPayload, plainPayload);
+      const dto = await this.salesACLService.mapCreateSalesProductPayloadToCreateInventoryItem(event);
+      await this.inventoryItemCreateService.runTransaction(dto);
     }
   }
 }
