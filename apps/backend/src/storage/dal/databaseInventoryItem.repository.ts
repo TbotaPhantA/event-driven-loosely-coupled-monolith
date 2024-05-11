594cdd83bc370fe5e8e5227cd832f2b2639b63c5
TbotaPhantA <botakiller@mail.ru>
2024-05-11 20:01:14 +0000

    implementation of new repository `getMany` method was added

--- apps/backend/src/storage/dal/databaseInventoryItem.repository.ts
+++ apps/backend/src/storage/dal/databaseInventoryItem.repository.ts
@@ -1,9 +1,25 @@
-import { InventoryItemRepository } from './inventoryItem.repository';
+import { GetManyParams, InventoryItemRepository } from './inventoryItem.repository';
 import { InventoryItem } from '../domain/inventoryItem/inventoryItem';
-import { EntityManager } from 'typeorm';
+import { EntityManager, Repository } from 'typeorm';
 import { InventoryItemMapper } from './inventoryItem.mapper';
+import { InjectRepository } from '@nestjs/typeorm';
+import { InventoryItemEntity } from './inventoryItem.entity';
 
 export class DatabaseInventoryItemRepository implements InventoryItemRepository {
+  constructor(
+    @InjectRepository(InventoryItemEntity)
+    private readonly dbRepo: Repository<InventoryItemEntity>,
+  ) {}
+
+  async getMany(params: GetManyParams): Promise<{ items: InventoryItem[]; total: number }> {
+    const [items, total] = await this.dbRepo.findAndCount({
+      take: params.limit,
+      skip: params.offset,
+    })
+
+    return { items: items.map(i => InventoryItemMapper.toDomain(i)), total };
+  }
+
   async insert(item: InventoryItem, manager: EntityManager): Promise<InventoryItem> {
     const entity = InventoryItemMapper.toEntity(item);
     const insertedEntity = await manager.save(entity);
