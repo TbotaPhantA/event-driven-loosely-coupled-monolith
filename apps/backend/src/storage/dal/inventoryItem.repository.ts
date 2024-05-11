7ebaea9050e4a18ad2aa11140bf70a127486c293
TbotaPhantA <botakiller@mail.ru>
2024-05-11 20:00:46 +0000

    new `getMany` method was added to repository

--- apps/backend/src/storage/dal/inventoryItem.repository.ts
+++ apps/backend/src/storage/dal/inventoryItem.repository.ts
@@ -1,6 +1,12 @@
 import { InventoryItem } from '../domain/inventoryItem/inventoryItem';
 import { ITransaction } from '../../infrastructure/transaction/shared/types/ITransaction';
 
+export interface GetManyParams {
+  limit: number;
+  offset: number;
+}
+
 export interface InventoryItemRepository {
+  getMany(params: GetManyParams): Promise<{ items: InventoryItem[], total: number }>;
   insert(item: InventoryItem, transaction: ITransaction): Promise<InventoryItem>;
 }
