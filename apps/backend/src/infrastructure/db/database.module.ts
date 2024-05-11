4b0729db94e87b01caeb49121b8d8dabaed52196
TbotaPhantA <botakiller@mail.ru>
2024-05-11 16:15:09 +0000

    new `InventoryItemEntity` was added

--- apps/backend/src/infrastructure/db/database.module.ts
+++ apps/backend/src/infrastructure/db/database.module.ts
@@ -4,6 +4,7 @@ import { config } from '../config/config';
 import { ProductEntity } from '../../sales/dal/product/product.entity';
 import { SalesProductRequestEntity } from '../idempotency/entities/salesProductRequest.entity';
 import { SalesProductMessage } from '../messages/entities/salesProductMessage.entity';
+import { InventoryItemEntity } from '../../storage/dal/inventoryItem.entity';
 
 @Module({
   imports: [
@@ -14,7 +15,7 @@ import { SalesProductMessage } from '../messages/entities/salesProductMessage.en
       username: config.database.username,
       password: config.database.password,
       database: config.database.database,
-      entities: [ProductEntity, SalesProductRequestEntity, SalesProductMessage],
+      entities: [ProductEntity, SalesProductRequestEntity, SalesProductMessage, InventoryItemEntity],
       synchronize: config.database.synchronize,
     }),
   ],
