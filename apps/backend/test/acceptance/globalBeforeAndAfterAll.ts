290356f604fb3e6107e40dc6e6176a4a78432267
TbotaPhantA <botakiller@mail.ru>
2024-06-12 23:59:58 +0000

    the following things were added: - producer - requester - cleaner

--- apps/backend/test/acceptance/globalBeforeAndAfterAll.ts
+++ apps/backend/test/acceptance/globalBeforeAndAfterAll.ts
@@ -1,42 +1,73 @@
-import { Test, TestingModule, TestingModuleBuilder } from '@nestjs/testing';
+import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import { Consumer, EachMessagePayload } from 'kafkajs';
+import { Consumer, EachMessagePayload, Kafka, Partitioners, Producer } from 'kafkajs';
 import { startConsumerFillingMessagePayloads } from '../shared/utils/messageBroker/startConsumerFillingMessagePayloads';
 import { TestApiController } from './testApi.controller';
 import { AppModule } from '../../src/app.module';
-import * as request from 'supertest';
+import { createKafka } from '../shared/utils/messageBroker/createKafka';
+import { config } from '../../src/infrastructure/config/config';
+import { Requester } from '../shared/utils/requests/requester';
+import { Cleaner } from '../shared/utils/cleaner';
+import { DataSource } from 'typeorm';
+import { Transport } from '@nestjs/microservices';
 
 let moduleRef: TestingModule;
 let consumer: Consumer;
 export let app: INestApplication;
-export const messagePayloads = new Array<EachMessagePayload>;
+export let messagePayloads: Array<EachMessagePayload>;
+export let kafka: Kafka;
+export let producer: Producer;
+export let requester: Requester;
+export let cleaner: Cleaner;
 
 beforeAll(async () => {
-  moduleRef = await createTestingModule().compile();
+  moduleRef = await Test.createTestingModule({
+    controllers: [TestApiController],
+    imports: [AppModule],
+  }).compile();
+
   app = moduleRef.createNestApplication();
+  messagePayloads = new Array<EachMessagePayload>;
+  kafka = createKafka();
+  producer = kafka.producer({ createPartitioner: Partitioners.DefaultPartitioner });
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: [
+          `${config.kafka.kafka1Host}:${config.kafka.kafka1ExternalPort}`,
+          `${config.kafka.kafka2Host}:${config.kafka.kafka2ExternalPort}`,
+          `${config.kafka.kafka3Host}:${config.kafka.kafka3ExternalPort}`,
+        ],
+        connectionTimeout: 10000,
+      },
+      consumer: {
+        groupId: config.kafka.consumerGroup,
+      },
+    },
+  }, { inheritAppConfig: true });
+
+  await app.startAllMicroservices();
+
   const [,startedConsumer] = await Promise.all([
-    app.init(),
-    startConsumerFillingMessagePayloads(messagePayloads),
+    app.listen(config.app.port),
+    startConsumerFillingMessagePayloads(kafka, messagePayloads),
+    producer.connect(),
   ])
+
   consumer = startedConsumer;
-
-  function createTestingModule(): TestingModuleBuilder {
-    return Test.createTestingModule({
-      controllers: [TestApiController],
-      imports: [AppModule],
-    })
-  }
-
+  requester = new Requester(app);
+  cleaner = new Cleaner(app.get(DataSource));
 }, 30000);
 
 afterAll(async () => {
-  await request(app.getHttpServer()).post('/test-api/clean-db')
   await Promise.all([
     moduleRef.close(),
     consumer.stop(),
   ])
   await Promise.all([
     consumer.disconnect(),
+    producer.disconnect(),
     app.close(),
   ])
 }, 30000);
