4f566b2ccce8793bbb204799f62e3d2bc51bff9b
TbotaPhantA <botakiller@mail.ru>
2024-06-12 23:59:02 +0000

    `createKafka` function was moved to separate file

---
+++ apps/backend/test/shared/utils/messageBroker/createKafka.ts
@@ -0,0 +1,13 @@
+import { Kafka } from 'kafkajs';
+import { config } from '../../../../src/infrastructure/config/config';
+
+export const createKafka = (): Kafka => {
+    return new Kafka({
+      clientId: 'acceptance-tests',
+      brokers: [
+        `${config.kafka.kafka1Host}:${config.kafka.kafka1ExternalPort}`,
+        `${config.kafka.kafka2Host}:${config.kafka.kafka2ExternalPort}`,
+        `${config.kafka.kafka3Host}:${config.kafka.kafka3ExternalPort}`,
+      ],
+    });
+  }
