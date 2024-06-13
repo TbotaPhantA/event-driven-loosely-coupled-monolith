d022ddbd235d97f5ba4131c019ffa5d2e1336265
TbotaPhantA <botakiller@mail.ru>
2024-06-13 00:01:03 +0000

    utility with all paths was added for Requester

---
+++ apps/backend/test/shared/utils/getAllSalesPaths.ts
@@ -0,0 +1,8 @@
+import { createProductResource, salesProductResource } from '../../../src/sales/application/shared/resources';
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function getAllSalesPaths() {
+  return {
+    createProductPath: `/${salesProductResource}/${createProductResource}`,
+  }
+}
