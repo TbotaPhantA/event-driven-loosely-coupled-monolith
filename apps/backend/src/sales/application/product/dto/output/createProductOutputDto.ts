aa614ec5a0e1f009b24ffda5132509eaacf27aa4
TbotaPhantA <botakiller@mail.ru>
2024-06-22 16:49:48 +0000

    creation by raw was moved to static method instead of constructor

--- apps/backend/src/sales/application/product/dto/output/createProductOutputDto.ts
+++ apps/backend/src/sales/application/product/dto/output/createProductOutputDto.ts
@@ -7,21 +7,22 @@ import { createProductLinksFrom } from './links/utils/createProductLinksFrom';
 
 export class CreateProductOutputDto {
   @ApiProperty({ type: ProductOutputDto })
-  product: ProductOutputDto;
+  product!: ProductOutputDto;
 
   @ApiProperty({ type: [Link] })
-  links: Link[];
+  links!: Link[];
 
-  constructor(raw: NoMethods<CreateProductOutputDto>) {
-    this.product = raw.product;
-    this.links = raw.links;
+  static createByRaw(raw: NoMethods<CreateProductOutputDto>): CreateProductOutputDto {
+    const dto = new CreateProductOutputDto();
+    dto.product = raw.product;
+    dto.links = raw.links;
+    return dto;
   }
 
   static from(product: Product): CreateProductOutputDto {
     const exported = product.export();
     const output = new ProductOutputDto(exported);
     const links = createProductLinksFrom(exported);
-
-    return new CreateProductOutputDto({ product: output, links });
+    return CreateProductOutputDto.createByRaw({ product: output, links });
   }
 }
