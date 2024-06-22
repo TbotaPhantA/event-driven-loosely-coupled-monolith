2e168ca0c66384c62b85b71a248c2d0cfe5b0e7c
TbotaPhantA <botakiller@mail.ru>
2024-06-22 15:34:16 +0000

    express middleware was changed to fastify

--- apps/backend/src/infrastructure/correlation/middleware/correlation-middleware.service.ts
+++ apps/backend/src/infrastructure/correlation/middleware/correlation-middleware.service.ts
@@ -1,14 +1,14 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { CorrelationService } from '../correlation.service';
 import { CORRELATION_ID_HEADER } from '../correlationConstants';
-import { Request, Response } from 'express';
+import { FastifyRequest, FastifyReply } from 'fastify';
 
 @Injectable()
 export class CorrelationMiddleware implements NestMiddleware {
   constructor(private readonly correlationService: CorrelationService) {}
 
-  public use(req: Request, res: Response, next: () => void): void {
-    const correlationIdHeader = req.get(CORRELATION_ID_HEADER);
+  public use(req: FastifyRequest['raw'], res: FastifyReply['raw'], next: () => void): void {
+    const correlationIdHeader = req.headers[CORRELATION_ID_HEADER];
 
     req.headers[CORRELATION_ID_HEADER] = this.correlationService.startNewCorrelationId(correlationIdHeader);
 
