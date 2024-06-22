eeefb7a8f65e74ff9b1b6721ee3904cd2b696d90
TbotaPhantA <botakiller@mail.ru>
2024-06-22 19:44:58 +0000

    adjustPricePath was added

--- apps/backend/test/shared/utils/getAllSalesPaths.ts
+++ apps/backend/test/shared/utils/getAllSalesPaths.ts
@@ -1,8 +1,13 @@
-import { createProductResource, salesProductResource } from '../../../src/sales/application/shared/resources';
+import {
+  adjustPriceResource,
+  createProductResource,
+  salesProductResource
+} from '../../../src/sales/application/shared/resources';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function getAllSalesPaths() {
   return {
     createProductPath: `/${salesProductResource}/${createProductResource}`,
+    adjustPricePath: `/${salesProductResource}/${adjustPriceResource}`,
   }
 }
