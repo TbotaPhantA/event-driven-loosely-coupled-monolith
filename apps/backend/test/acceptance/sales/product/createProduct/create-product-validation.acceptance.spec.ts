42c1e6774f4dc90f1c5b4a53e389a735ded803b7
TbotaPhantA <botakiller@mail.ru>
2024-06-22 20:04:18 +0000

    graceful teardown was added

--- apps/backend/test/acceptance/sales/product/createProduct/create-product-validation.acceptance.spec.ts
+++ apps/backend/test/acceptance/sales/product/createProduct/create-product-validation.acceptance.spec.ts
@@ -1,25 +1,32 @@
 import { ProductController } from '../../../../../src/sales/application/product/product.controller';
 import { Requester } from '../../../../shared/utils/requests/requester';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
-import { Test } from '@nestjs/testing';
+import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../../../../../src/app.module';
 import { CreateProductBuilder } from '../../../../shared/__fixtures__/builders/commands/createProduct.builder';
 import { HttpStatus } from '@nestjs/common';
 import { SETUP_TIMEOUT } from '../../../../shared/constants';
 
 describe(`${ProductController.name} validation`, () => {
+  let moduleRef: TestingModule;
+  let app: NestFastifyApplication;
   let requester: Requester;
 
   beforeAll(async () => {
-    const moduleRef = await Test.createTestingModule({
+    moduleRef = await Test.createTestingModule({
       controllers: [],
       imports: [AppModule],
     }).compile();
-    const app = moduleRef.createNestApplication<NestFastifyApplication>(new FastifyAdapter());
+    app = moduleRef.createNestApplication<NestFastifyApplication>(new FastifyAdapter());
     requester = new Requester(app);
 
     await app.init();
     await app.getHttpAdapter().getInstance().ready();
+  }, SETUP_TIMEOUT)
+
+  afterAll(async () => {
+    await moduleRef.close();
+    await app.close();
   }, SETUP_TIMEOUT)
 
   describe(`${ProductController.prototype.createProduct.name} validation`, () => {
