359902bdad97e810bfdfd636cdd702f7939fb884
TbotaPhantA <botakiller@mail.ru>
2024-06-22 16:51:24 +0000

    messagesHelper.ts related objects was moved to messageHelper

--- apps/backend/test/shared/utils/helpers/messagesHelper.ts
+++ apps/backend/test/shared/utils/helpers/messagesHelper.ts
@@ -1,23 +1,26 @@
-import { Consumer, EachMessagePayload, Kafka } from 'kafkajs';
+import { Consumer, EachMessagePayload, Kafka, Partitioners, Producer } from 'kafkajs';
 import { extractMessage, Message } from '../../../../src/infrastructure/shared/utils/extractMessage';
 import { waitForMatchingPayload } from '../messageBroker/waitForMatchingPayload';
 import { config } from '../../../../src/infrastructure/config/config';
+import { createKafka } from '../messageBroker/createKafka';
 
 export class MessagesHelper {
   private messagePayloads: Array<EachMessagePayload>;
+  private kafka: Kafka;
+  private producer: Producer;
 
   constructor() {
     this.messagePayloads = new Array<EachMessagePayload>;
+    this.kafka = createKafka();
+    this.producer = this.kafka.producer({ createPartitioner: Partitioners.DefaultPartitioner });
   }
 
   async getMessageByCorrelationId(correlationId: string): Promise<Message> {
     return extractMessage(await waitForMatchingPayload(this.messagePayloads, correlationId))
   }
 
-  async startConsumerFillingMessagePayloads(
-    kafka: Kafka,
-  ): Promise<Consumer> {
-    const consumer = kafka.consumer({ groupId: config.kafka.consumerGroup });
+  async startConsumerFillingMessagePayloads(): Promise<Consumer> {
+    const consumer = this.kafka.consumer({ groupId: config.kafka.consumerGroup });
     await consumer.connect();
     await consumer.subscribe({
       topic: config.kafka.kafkaProductsTopic,
