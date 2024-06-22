b17062e73d7de11daf1d97b9889c9f72fad5f29c
TbotaPhantA <botakiller@mail.ru>
2024-06-22 16:51:50 +0000

    requester was changed for fastify

--- apps/backend/test/shared/utils/requests/requester.ts
+++ apps/backend/test/shared/utils/requests/requester.ts
@@ -1,10 +1,11 @@
-import { HttpStatus, INestApplication } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common';
 import { CreateProduct } from '../../../../src/sales/domain/product/commands/createProduct';
-import * as request from 'supertest';
 import { CORRELATION_ID_HEADER } from '../../../../src/infrastructure/correlation';
 import { getAllSalesPaths } from '../getAllSalesPaths';
 import { ulid } from 'ulid';
 import { CreateProductOutputDto } from '../../../../src/sales/application/product/dto/output/createProductOutputDto';
+import { NestFastifyApplication } from '@nestjs/platform-fastify';
+import { plainToInstance } from 'class-transformer';
 
 interface CreateProductParams {
   dto: CreateProduct,
@@ -15,7 +16,7 @@ export class Requester {
   private readonly paths: ReturnType<typeof getAllSalesPaths>;
 
   constructor(
-    private readonly app: INestApplication,
+    private readonly app: NestFastifyApplication,
   ) {
     this.paths = getAllSalesPaths();
   }
@@ -23,9 +24,18 @@ export class Requester {
   async createProduct(params: CreateProductParams): Promise<{ body: CreateProductOutputDto, status: HttpStatus }> {
     const correlationId = params.correlationId ?? ulid();
 
-    return request(this.app.getHttpServer())
-      .post(this.paths.createProductPath)
-      .set(CORRELATION_ID_HEADER, correlationId)
-      .send(params.dto)
+    const result = await this.app
+      .inject({
+        method: 'POST',
+        url: this.paths.createProductPath,
+        headers: {
+          [CORRELATION_ID_HEADER]: correlationId,
+        },
+        body: params.dto
+      });
+
+    const body = plainToInstance(CreateProductOutputDto, JSON.parse(result.body) as CreateProductOutputDto);
+
+    return { body, status: result.statusCode };
   }
 }
