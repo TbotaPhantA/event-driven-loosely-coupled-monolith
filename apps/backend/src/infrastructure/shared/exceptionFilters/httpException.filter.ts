231373781bcc67d29f6893d78869fbef0c8cfb4c
TbotaPhantA <botakiller@mail.ru>
2024-06-22 15:34:58 +0000

    exception filter was changed for fastify

--- apps/backend/src/infrastructure/shared/exceptionFilters/httpException.filter.ts
+++ apps/backend/src/infrastructure/shared/exceptionFilters/httpException.filter.ts
@@ -1,21 +1,21 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
-import { Request, Response } from 'express';
 import {
   ProductAlreadyCreatedException,
 } from '../../../sales/application/product/exceptions/productAlreadyCreatedException';
+import { FastifyRequest, FastifyReply } from 'fastify';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost): void {
     const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
+    const request = ctx.getRequest<FastifyRequest>();
+    const reply = ctx.getResponse<FastifyReply>();
     const status = exception.getStatus();
 
     if (exception instanceof ProductAlreadyCreatedException) {
-      response
+      reply
         .status(status)
-        .json({
+        .send({
           message: exception.message,
           statusCode: status,
           timestamp: new Date().toISOString(),
@@ -24,9 +24,9 @@ export class HttpExceptionFilter implements ExceptionFilter {
           links: exception.links,
         });
     } else {
-      response
+      reply
         .status(status)
-        .json({
+        .send({
           message: exception.message,
           statusCode: status,
           timestamp: new Date().toISOString(),
