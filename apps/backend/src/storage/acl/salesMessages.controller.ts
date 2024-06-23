be11191d84ecd8825e7b305e15eba6dd09d8fb1f
TbotaPhantA <botakiller@mail.ru>
2024-06-23 15:31:37 +0000

    `MessageNames` enum was renamed to `MessageNamesEnum`

--- apps/backend/src/storage/acl/salesMessages.controller.ts
+++ apps/backend/src/storage/acl/salesMessages.controller.ts
@@ -2,7 +2,7 @@ import { Controller } from '@nestjs/common';
 import { Ctx, EventPattern, KafkaContext } from '@nestjs/microservices';
 import { config } from '../../infrastructure/config/config';
 import { getMessageByContext } from '../../infrastructure/shared/utils/getMessageByContext';
-import { MessageNames } from '../application/shared/enums/messageNames.enum';
+import { MessageNamesEnum } from '../application/shared/enums/messageNames.enum';
 import { validatePayload } from '../../infrastructure/shared/utils/validatePayload';
 import {
   CreateSalesProductEventPayload
@@ -22,7 +22,7 @@ export class SalesMessagesController {
     const message = getMessageByContext(context);
     const plainPayload = JSON.parse(message.value.payload);
 
-    if (message.headers.messageName === MessageNames.ProductCreated) {
+    if (message.headers.messageName === MessageNamesEnum.ProductCreated) {
       const event = await validatePayload(CreateSalesProductEventPayload, plainPayload);
       const dto = await this.salesACLService.mapCreateSalesProductPayloadToCreateInventoryItem(event);
       await this.inventoryItemCreateService.runTransaction(dto);
