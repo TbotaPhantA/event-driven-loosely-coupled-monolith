e3375c45c2072d83832b62dc01e402c36248a84d
TbotaPhantA <botakiller@mail.ru>
2024-06-23 15:10:29 +0000

    asserIsNotUndefined utility was implemented

---
+++ apps/backend/test/shared/utils/isNotUndefined.ts
@@ -0,0 +1,5 @@
+export function assertIsNotUndefined<T>(something: T | undefined): asserts something is T {
+  if (something !== undefined) {
+    throw new Error('Assert is not undefined has failed!');
+  }
+}
