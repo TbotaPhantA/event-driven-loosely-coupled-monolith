a06f61fb15d2d15b9a5d148445acb399f2226698
TbotaPhantA <botakiller@mail.ru>
2024-08-08 18:11:22 +0000

    new salesProductIdempMessagesService was added

--- apps/backend/src/infrastructure/idempotency/idempotency.module.ts
+++ apps/backend/src/infrastructure/idempotency/idempotency.module.ts
@@ -1,17 +1,20 @@
 import { Module, Provider } from '@nestjs/common';
 import { SalesProductIdempotencyService } from './services/salesProductIdempotency.service';
-import {
-  DatabaseSalesProductIdempotentRequestRepository
-} from './repositories/databaseSalesProductIdempotentRequestRepository';
-import { SALES_PRODUCT_IDEMPOTENCY_SERVICE } from './constants';
+import { SALES_PRODUCT_IDEMPOTENCY_SERVICE, SALES_PRODUCT_IDEMP_MESSAGES_SERVICE } from './constants';
+import { SalesProductIdempMessagesService } from './services/salesProductIdempMessages.service';
 
 const salesProductIdempotencyServiceProvider: Provider = {
   provide: SALES_PRODUCT_IDEMPOTENCY_SERVICE,
   useClass: SalesProductIdempotencyService,
 }
 
+const salesProductMessageIdempServiceProvider: Provider = {
+  provide: SALES_PRODUCT_IDEMP_MESSAGES_SERVICE,
+  useClass: SalesProductIdempMessagesService,
+}
+
 @Module({
-  providers: [salesProductIdempotencyServiceProvider, DatabaseSalesProductIdempotentRequestRepository],
-  exports: [salesProductIdempotencyServiceProvider],
+  providers: [salesProductIdempotencyServiceProvider, salesProductMessageIdempServiceProvider],
+  exports: [salesProductIdempotencyServiceProvider, salesProductMessageIdempServiceProvider],
 })
 export class IdempotencyModule {}
